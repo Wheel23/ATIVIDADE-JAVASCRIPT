7896682dbeb7f465f59384e3503b33968a576cd6
Luiz <162443980+Wheel23@users.noreply.github.com>
2024-06-12 08:36:25 -0300

    Add files via upload

---
+++ ATIVIDADES.js
@@ -0,0 +1,39 @@
+
+var a = 1, b = 2, c = 3
+
+function boasvindas(){
+console.log("BOAS VINDAS")
+
+}
+
+function somarnu(){
+    
+    console.log("sua soma é: " + (a + b))
+}
+
+function subtrairnu(){
+   
+    console.log("sua subtração é: " + (a - b))
+}
+
+function multinu(){
+   
+    console.log("sua multiplicação é: " + (a * b))
+}
+
+function divisaonu(){
+   
+    console.log("sua divisão é :" + (a / b))
+}
+
+function médianu(){
+   
+    console.log("sua média é :" + ((a+b+c)/3))
+}
+
+boasvindas()
+somarnu()
+subtrairnu()
+multinu()
+divisaonu()
+médianu()
